b8a4eea3bc22f5280c7b1d9ad6c080fc96941e46
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-09 17:59:37 +0530

    Time: 5 ms (36.95%) | Memory: 7.7 MB (51.52%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(vector<int>& nums, vector<int>v, int s){
-    //base cacse
+    void solve(vector<int>& nums, vector<int>&v, int s){
+        ans.push_back(v); // above base case or it would forget soe cases
+//base cacse  
         if(s == nums.size()){
-            sort(v.begin(), v.end());
-            if(find(ans.begin(), ans.end(), v) == ans.end())
-            ans.push_back(v);
             return;
         }     
     //rec call
-        //exclude
-        solve(nums, v, s+1);
-        
-        //include
-        v.push_back(nums[s]);
-        solve(nums, v, s+1);
-        v.pop_back();
+        for(int i = s; i < nums.size() ; i++){
+            if( s == i || nums[i] != nums[i-1]){ //to avoid duplication ar[i] != ar[i-1] but work if i and s are equal for an exception
+                v.push_back(nums[i]);
+                solve(nums, v, i+1);
+                v.pop_back();
+            }
+        }
         return;
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> v;
-        ans.push_back(v);
+        sort(nums.begin(), nums.end());
         solve(nums, v, 0);
-        sort(ans.begin(), ans.end());
         return ans;
     }
 };
