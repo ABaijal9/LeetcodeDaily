51a96114ba064cf2d449d84cfd64deea201e74f6
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-09 16:14:17 +0530

    Time: 5 ms (20.66%) | Memory: 10.8 MB (43.81%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(vector<int>& nums, vector<int> v, int in , int n){
+    void solve(vector<int>& nums, vector<int>&v, int in , int n){
         //base case
         if(in == n){ 
          //   if(find(ans.begin(), ans.end(), v ) == ans.end()) ans.push_back(v);
