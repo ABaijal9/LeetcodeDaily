d24b5cc9ae36bfac286a70ff94c1cb6cbb200de0
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-09 11:48:24 +0530

    Time: 3 ms (54.68%) | Memory: 15.9 MB (25.38%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>& nums, vector<int> v, int in , int n){
+        //base case
+        if(in == n){ 
+            if(find(ans.begin(), ans.end(), v ) == ans.end()) ans.push_back(v);
+           // ans.push_back(v);
+            return;
+        }
+        //rec case
+        //exclude krege
+            solve(nums, v,  in+1 , n);
+         //include krege
+           // if(find(v.begin(), v.end(), nums[in] ) == v.end()) 
+            v.push_back(nums[in]);
+            solve(nums, v, in+1 , n);
+            
+            return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> v;
+        solve(nums, v, 0 , nums.size());
+        return ans;
+    }
+};
