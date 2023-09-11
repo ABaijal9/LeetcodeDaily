a12ef3a01729a7af4ec24465030deab51795b30a
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-11 15:21:27 +0530

    Time: 346 ms (24.80%) | Memory: 17.9 MB (15.56%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-int jump(vector<int>& nums){
+int ans = 0 ;
+long long sol(vector<int>& nums, int n, int currpos, vector<int> &dp){
+    //base case
 
+    if( currpos == n-1 ){
+            return 0;
+    }
+    if(dp[currpos] != -1) return dp[currpos]; 
+    //calculation
+    long long least = INT_MAX;
+    
+    for(int i = 1 ; i <= nums[currpos]; i++){
+        if( i + currpos > n-1) break;
+      // if(nums[currpos+i] != 0){
+            least = min(least, 1+sol(nums, n, i + currpos , dp));
+      // }
+    }
+    return dp[currpos] = least;
+}
+int jump(vector<int>& nums){
     int n= nums.size();
-    if( n == 1) return 0;
- 
-    int reach = 0 ;
-    int rangemax = 0;
-    int ans = 0;
-    for(int currpos=0; currpos<n ; currpos++){
-        rangemax = max(rangemax, nums[currpos] + currpos);
-        if(rangemax >= n-1){
-            ans++;
-            return ans;
-        }
-        else if(currpos == reach){
-          //1: make reach = rangemax (to expand reach)
-          reach = rangemax;
-          //2: add 1 more jump to ans
-          ans++;
-      }
-    } 
-        return ans;
+    vector<int> dp(n,-1);
+    return sol(nums, n, 0 , dp);
     }
 };
