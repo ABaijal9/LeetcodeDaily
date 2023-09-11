8fab2e2e696efe8298bd01a757f2f80466d464fc
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-11 13:44:34 +0530

    Time: 7 ms (94.76%) | Memory: 16.4 MB (96.05%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
 int jump(vector<int>& nums){
-    int steps = 0 , max_reach = 0 , jumps = 0;
-    
-    for(int i = 0 ; i < nums.size()-1; i++){
-      max_reach = max( max_reach , nums[i] + i );
-        if( i == steps){
-          steps = max_reach;
-          jumps++;
+
+    int n= nums.size();
+    if( n == 1) return 0;
+ 
+    int reach = 0 ;
+    int rangemax = 0;
+    int ans = 0;
+    for(int currpos=0; currpos<n ; currpos++){
+        rangemax = max(rangemax, nums[currpos] + currpos);
+        if(rangemax >= n-1){
+            ans++;
+            return ans;
+        }
+        else if(currpos == reach){
+          //1: make reach = rangemax (to expand reach)
+          reach = rangemax;
+          //2: add 1 more jump to ans
+          ans++;
       }
     } 
-      return jumps;
+        return ans;
     }
 };
