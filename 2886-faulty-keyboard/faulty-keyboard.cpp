7d6e6cf6b4c9614796d1c4460b0fe7c0ebbf3ea6
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-09-15 11:30:39 +0530

    Time: 3 ms (86.02%) | Memory: 6.8 MB (16.28%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    void sol(string& s, int pos, int n){
-        if( pos >= n) return;
-        
-        if(s[pos] == 'i'){
-            reverse(s.begin(), s.begin()+pos);
-            s.erase(s.begin() + pos);
-            return sol(s, pos,n);
-        }
-        return sol(s, pos+1, n);
-    }
     
     string finalString(string s){
-        sol(s,0,s.size());
-        return s;      
+        string t = "";
+        for(auto it: s){
+            if(it != 'i') t+= it;
+            else reverse(t.begin(), t.end());
+        }
+        return t;      
     }
 };
