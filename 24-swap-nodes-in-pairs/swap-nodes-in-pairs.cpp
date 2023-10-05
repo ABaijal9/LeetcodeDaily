b874eb90bf3d3334df9678da58c403c2d4c6aabf
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-10-05 14:51:51 +0530

    Time: 4 ms (43.49%) | Memory: 7.8 MB (26.86%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -5,24 +5,23 @@ public:
             return head;
         }
 
-        ListNode* newHead = head->next;  // New head after swapping.
-        ListNode* prev = nullptr;
+        ListNode* first = head;
+        ListNode* second = head->next;
+        ListNode* newHead = second;
 
-        while (head != nullptr && head->next != nullptr) {
-            ListNode* first = head;
-            ListNode* second = head->next;
-
-            // Swap nodes.
-            first->next = second->next;
+        while (first != nullptr && second != nullptr) {
+            ListNode* temp = second->next;
             second->next = first;
 
-            if (prev != nullptr) {
-                prev->next = second;  // Update previous node's next pointer.
+            if (temp == nullptr || temp->next == nullptr) {
+                // End of the list or only one node left
+                first->next = temp;
+                break;
             }
 
-            prev = first;  // Move the prev pointer to the next pair.
-
-            head = first->next;  // Move to the next pair.
+            first->next = temp->next;
+            first = temp;
+            second = temp->next;
         }
 
         return newHead;
