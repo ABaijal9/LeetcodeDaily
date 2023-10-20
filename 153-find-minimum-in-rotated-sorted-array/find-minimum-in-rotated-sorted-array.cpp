0d85de10379c95d02e208d803aba559569f7ec5e
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2023-10-20 10:51:18 +0530

    Time: 6 ms (22.61%) | Memory: 10.5 MB (35.48%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -3,14 +3,20 @@ public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
         if(n == 1)return nums[0];
-        int i = 0;
-        //j = n-1;
-        if(nums[i] < nums[n-1]) return nums[i];
-         //check which way to check: left to right or right to left
+        int i = 0 , j = n-1;
+        int k = 0;
+        
+        while( i <= j ){
+            if( nums[i] <= nums[j] ) return nums[i];
+        //check which way to check: left to right or right to left
         // logic used same as monotonic array
-        i = n-1;
-        while(nums[i-1] < nums[i] && i >= 0) i--;
+            k = i+(j-i)/2;  //mistake k = n/2 do it by j/2
+            if( nums[k] > nums[j] ) i = k+1;
+            else{ j = k; }
+           // else if(nums[k] < nums[k]) j = k;
+    //    while(nums[i-1] < nums[i] && i >= 0) i--;
+        
+        }
         return nums[i];
-
     }
 };
