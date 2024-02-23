0e677b3d849b91a135aba9385a4b0e309abb9505
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2024-02-23 18:21:30 +0530

    Time: 4 ms (59.85%) | Memory: 13.9 MB (72.92%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,32 +1,61 @@
-class Solution {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {  
+    //      map<col,map<row, val>>;
     map<int, map<int, multiset<int>>> mp;
-
+    
 public:
-    void solve(TreeNode* root, int row, int col) {
-        if (root == NULL) return;
-
-        mp[col][row].insert(root->val);
-
-        // preorder
-        solve(root->left, row + 1, col - 1);
-        solve(root->right, row + 1, col + 1);
-        return;
-    }
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>> v;
-        solve(root, 0, 0);
+        queue<pair<TreeNode*, pair<int,int>>> q;
+        q.push({root,{0,0}});
+        q.push({NULL,make_pair(0,0)});
 
-        for (auto& col : mp) {
-            vector<int> colVals;
-            for (auto& row : col.second) {
-                for (auto& val : row.second) {
-                    colVals.push_back(val);
+        while(!q.empty()){
+            auto f = q.front();
+            q.pop();
+
+            TreeNode* t = f.first;
+            int col = f.second.first;
+            int row = f.second.second;
+
+            if(t == NULL){
+                if(!q.empty()) q.push({NULL,{0,0}});
+            }
+            else{
+                mp[col][row].insert(t->val);
+                if(t->left){
+                    pair<TreeNode*, pair<int,int>> p;
+                    p.first = t->left;
+                    p.second = make_pair(col-1,row+1);
+                    q.push(p);
+                }
+                if(t->right){
+                    pair<TreeNode*, pair<int,int>> p;
+                    p.first = t->right;
+                    p.second = make_pair(col+1,row+1);
+                    q.push(p);
                 }
             }
-            v.push_back(colVals);
         }
-
-        return v;
-    }
+           for(auto& col : mp){
+               vector<int> colval;
+               for(auto& row : col.second){
+                   for(auto& val : row.second){
+                       colval.push_back(val);
+                   }
+               }
+               v.push_back(colval);
+           }
+            return v;
+        }
 };
