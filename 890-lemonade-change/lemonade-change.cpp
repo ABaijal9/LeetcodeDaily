0921a3a6ab08e8a666ec422e25f5c89270ebf8c3
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2024-03-25 19:34:58 +0530

    Time: 75 ms (48.07%) | Memory: 86.9 MB (6.82%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int,int> cash;
+        cash[20] = 0;
+        cash[10] = 0;
+        cash[5] = 0;
+        for(auto charge: bills){
+            if(charge != 5){
+                if(charge == 20){
+                    cout << cash[10] << " " << cash[5] << endl;
+                    if(cash[10] < 1 and cash[5] < 3){ // 10 and 3 both not present
+                        cout << "1";
+                        return false; }
+                    else if(cash[10] < 1 and cash[5] >= 3){ // 10 not present and 3 present
+                        cash[5] -= 3;
+                    }
+                    else if(cash[10] > 1 and cash[5] < 1){ // 10 present and 3 not present
+                        cout << "2";
+                        return false;
+                    }
+                    else{ // both present
+                        cash[10] -= 1;
+                        cash[5] -= 1;
+                    }
+
+                }
+                else if(charge == 10){
+                    if(cash[5] < 1){
+                        cout << "3";
+                    return false;}
+                    cash[5] -= 1;
+                } }
+            cash[charge] += 1; 
+        }    
+      return true;  
+    }
+};
