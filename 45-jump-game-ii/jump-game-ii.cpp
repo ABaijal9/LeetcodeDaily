0518f0032c3d18d6fab04f5b227827fc4f1f2c17
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2024-03-27 17:38:03 +0530

    Time: 7 ms (89.81%) | Memory: 18.8 MB (87.70%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
 int ans = 0 ;
-long long sol(vector<int>& nums, int n, int currpos, vector<int> &dp){
-    //base case
-
-    if( currpos == n-1 ){
-            return 0;
-    }
-    if(dp[currpos] != -1) return dp[currpos]; 
-    //calculation
-    long long least = INT_MAX;
+int jump(vector<int>& nums ){
+    int n = nums.size();
+    int start = 0, end = 0;
     
-    for(int i = 1 ; i <= nums[currpos]; i++){
-        if(i + currpos > n-1 ){
-            break;
-        }
-        least = min(least, 1+sol(nums, n, i + currpos , dp));
+    while(end < n-1){
+        int farthest = 0;
+        for(int i = start; i <= end; i++) 
+        farthest = max(farthest, nums[i] + i);
+
+        start = end+1;
+        end = farthest;
+        ans += 1;
     }
-    return dp[currpos] = least;
-}
-int jump(vector<int>& nums){
-    int n= nums.size();
-    vector<int> dp(n,-1);
-    return sol(nums, n, 0 , dp);
+    return ans;
     }
 };
