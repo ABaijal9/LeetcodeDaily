ea09cc579884d79ae036378543241062df311aef
Ayushi Baijal <94740587+ABaijal9@users.noreply.github.com>
2025-04-20 22:53:14 +0530

    Time: 5 ms (20.17%) | Memory: 12.6 MB (65.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def reverseWords(self, s):
-        l = s.split()
-
-        return " ".join(reversed(l))
+        ans = []
+        temp = ""
+        for ch in s:
+            if ch == ' ':
+                if temp:
+                    ans.insert(0,temp)
+                    temp = ""
+                continue
+            temp += ch
+        if temp:
+            ans.insert(0,temp)
+        
+        res = ""
+        for i in ans:
+            res = res + ' ' + i
+        return res[1:]
